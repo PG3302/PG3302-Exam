e47796e250f60646144b0c0ffe931e398f1c9a97
Vegard Falldalen <vegfall@gmail.com>
2023-11-03 14:47:31 +0100

    Created logger singleton. --Only logs to console now

---
+++ TravelPlanner/TravelPlannerApp/Logger.cs
@@ -0,0 +1,37 @@
+﻿namespace TravelPlanner.TravelPlannerApp
+{
+    //Singleton design pattern from https://csharpindepth.com/Articles/Singleton
+    internal class Logger
+    {
+        private static Logger? _instance = null;
+        private static readonly object _constructLock = new();
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetLogger()
+        {
+            if (_instance == null)
+            {
+                lock (_constructLock)
+                {
+                    _instance ??= new();
+                }
+            }
+
+            return _instance;
+        }
+
+        public static void LogError(string message, Exception? exception = null)
+        {
+            Console.WriteLine($"[Error]\t{exception?.Message} -- {message}");
+        }
+
+        public static void LogInfo(string message)
+        {
+            Console.WriteLine($"[Info]\t{message}");
+        }
+    }
+}
--- TravelPlanner/TravelPlannerApp/TravelPlanner.cs
+++ TravelPlanner/TravelPlannerApp/TravelPlanner.cs
@@ -1,10 +1,32 @@
-﻿namespace TravelPlanner.TravelPlannerApp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlanner.TravelPlannerApp
 {
     internal class TravelPlanner
     {
         public void Start()
         {
             Console.WriteLine("I am the main program.");
+
+            ErrorCreator();
+        }
+
+        public void ErrorCreator()
+        {
+            int[] tooShort = new int[1];
+            int longEnough = 0;
+
+            try
+            {
+                longEnough = tooShort[1];
+            } catch(IndexOutOfRangeException error)
+            {
+                Logger.LogError("Personal message for each exception.", error);
+            }
         }
     }
 }

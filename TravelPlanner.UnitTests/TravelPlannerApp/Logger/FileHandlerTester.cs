0b1b26e8afb87e6f916fcf5ec45ef84265ab0d3e
Vegard Falldalen <vegfall@gmail.com>
2023-11-06 10:50:04 +0100

    Added FileHandler unit tests

---
+++ TravelPlanner.UnitTests/TravelPlannerApp/Logger/FileHandlerTester.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using TravelPlanner.TravelPlannerApp.Logger;
+
+namespace TravelPlanner.UnitTests.TravelPlannerApp.Logger
+{
+    internal class FileHandlerTester
+    {
+        private static readonly string folderLocation = Directory.GetCurrentDirectory();
+        private static readonly string fileName = "FileHandlerTest.txt";
+        private static readonly string fullPath = Path.Combine(folderLocation, fileName);
+
+        [SetUp]
+        public void Setup_CreateNewFile()
+        {
+            FileHandler.WriteToFile("", fullPath.ToString(), false);
+        }
+
+        [TearDown]
+        public void TearDown_DeleteFile()
+        {
+            FileHandler.DeleteFile(fullPath);
+        }
+
+        [Test]
+        public void WriteToFile_CreateNewFile_FileIsCreated()
+        { 
+            Assert.That(File.Exists(fileName), Is.True);
+        }
+
+        [Test]
+        public void WriteToFile_AddText_TextIsAdded()
+        {
+            FileHandler.WriteToFile("X", fullPath.ToString(), false);
+            string? textFromFile = FileHandler.ReadFromFile(fullPath);
+
+            Assert.That(textFromFile, Does.Contain('X'));
+        }
+    }
+}
---
+++ TravelPlanner.UnitTests/TravelPlannerApp/Logger/LoggerTester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlanner.UnitTests.TravelPlannerApp.Logger
+{
+    internal class LoggerTester
+    {
+    }
+}

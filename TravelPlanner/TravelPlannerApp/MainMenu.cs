63842221fb84eeeb7d0ca2de85804d0e7e398d87
Sebastian Edvardsen <sebastian_edvardsen@hotmail.com>
2023-11-09 13:27:09 +0100

    Fixed booting issues

--- TravelPlanner/TravelPlannerApp/MainMenu.cs
+++ TravelPlanner/TravelPlannerApp/MainMenu.cs
@@ -1,43 +1,57 @@
-public class MainMenu
+namespace TravelPlanner.TravelPlannerApp
 {
-    public void DisplayMenu()
+    public class MainMenu
     {
-        while(true)
+        public void DisplayMenu()
         {
-            Console.Clear();
-            Console.WriteLine("Travelplanner hehe xd:");
-            Console.WriteLine("1. Login");
-            Console.WriteLine("2. Search");
-            Console.WriteLine("3. Exit");
-
-            if(int.TryParse(Console.ReadLine(), out int selectedOption))
+            while (true)
             {
-                if(selectedOption == 1)
-                    ProcessOption("Option 1. Login Page"); // Login?
-                else if(selectedOption == 2)
-                    ProcessOption("Option 2 Search Page"); //  Søkemotor?
-                else if(selectedOption == 3)
+                //Console.Clear();
+                Console.WriteLine("Travelplanner hehe xd:");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Search");
+                Console.WriteLine("3. Exit");
+
+                if (int.TryParse(Console.ReadLine(), out int selectedOption))
                 {
-                    Console.WriteLine("Exiting program");
-                break;
+                    if (selectedOption == 1)
+                        ProcessOption("Option 1. Login Page"); // Login?
+                    else if (selectedOption == 2)
+                        ProcessOption("Option 2 Search Page"); //  Søkemotor?
+                    else if (selectedOption == 3)
+                    {
+                        Console.WriteLine("Exiting program");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Invalid Option");
+
                 }
+
                 else
-                    Console.WriteLine("Invalid Option");
+                    Console.WriteLine("Invalid Option.");
 
+                Console.WriteLine("Press Enter to continue..");
+                Console.ReadLine();
             }
-
-            else
-                Console.WriteLine("Invalid Option.");
-
-            Console.WriteLine("Press Enter to continue..");
-            Console.ReadLine();
         }
-    }
 
-    private static void ProcessOption()
-    {
-       message = "test";
-       Console.WriteLine(message);
-        // logic for valgt option
+        private static void ProcessOption(string message)
+        {
+            Console.WriteLine(message);
+
+            switch (message)
+            {
+                case "1. Login":
+                    Console.WriteLine("Her kommer login siden");
+                    break;
+                case "2. Search":
+                    Console.WriteLine("Her kommer søkefunksjon? el/annet");
+                    break;
+                default:
+                    Console.WriteLine("Hmm, noe funky");
+                    break;
+            }
+        }
     }
 }

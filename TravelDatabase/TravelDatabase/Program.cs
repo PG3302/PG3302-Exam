dfaf459dd612afc36c22ee75510df484dcffc2cc
LHL <lola003@student.kristiania.no>
2023-11-09 18:27:17 +0100

    Added Method Repos

--- TravelDatabase/TravelDatabase/Program.cs
+++ TravelDatabase/TravelDatabase/Program.cs
@@ -5,62 +5,13 @@ using TravelDatabase.Entities;
 using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
+using TravelDatabase.Repositories;
 
 namespace TravelDatabase
 {
     public class Program {
 		static void Main() {
-			InitFromCsv();
-
-		}
-
-		static void InitFromCsv() {
-			CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture) {
-				HasHeaderRecord = true
-			};
-			using (StreamReader reader = new StreamReader(@"Resources\CountryCapitals.csv"))
-			using (CsvReader csv = new CsvReader(reader , config)) {
-				IEnumerable<Capital> records = csv.GetRecords<CsvRow>().Select(row => new Capital() {
-					CapitalName = row.CapitalName ,
-					Continent = row.ContinentName ,
-					Longitude = row.CapitalLongitude ??0, //substitutes with 0 if value is NULL
-					Latitude = row.CapitalLatitude ??0,
-				});
-
-				using (TravelDbContext db = new()) {
-
-					List<Capital> items = records.ToList();
-
-					db.Capital.AddRange(items);
-					db.SaveChanges();
-
-					Console.WriteLine(DateTime.Now);
-				}
-			}
-		}
-		public static int AddUser(string username, Capital city, int admin) {
-			if (admin == 0 || admin == 1) {
-				User user = new();{
-					user.Name = $"{username}";
-					user.CityId = city.Id; //needs city selected from Capital table
-					user.Admin = admin;
-				}
-				using TravelDbContext db = new();
-				db.User.Add(user);
-				db.SaveChanges();
-
-				return user.Id;
-			}throw new Exception("Admin value not allowed");
-		}
-		static void AddTrip(int userId , int departlocation , int arrivalLocation) {
-			using TravelDbContext db = new();
-			Trip trip = new() {
-				UserId = userId ,
-				DepartureId = departlocation ,
-				ArrivalId = arrivalLocation ,
-			};
-			db.Add(trip);
-			db.SaveChanges();
+			InitDatabases.InitFromCsv();
 		}
 	}
 }
---
+++ TravelDatabase/TravelDatabase/Repositories/InitDatabases.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelDatabase.DataAccess.SqLite;
+using TravelDatabase.Entities;
+
+namespace TravelDatabase.Repositories {
+	public class InitDatabases {
+		public static void InitFromCsv() {
+			CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture) {
+				HasHeaderRecord = true
+			};
+			using (StreamReader reader = new StreamReader(@"Resources\CountryCapitals.csv"))
+			using (CsvReader csv = new CsvReader(reader , config)) {
+				IEnumerable<Capital> records = csv.GetRecords<CsvRow>().Select(row => new Capital() {
+					CapitalName = row.CapitalName ,
+					Continent = row.ContinentName ,
+					Longitude = row.CapitalLongitude ?? 0 , //substitutes with 0 if value is NULL
+					Latitude = row.CapitalLatitude ?? 0 ,
+				});
+
+				using (TravelDbContext db = new()) {
+
+					List<Capital> items = records.ToList();
+
+					db.Capital.AddRange(items);
+					db.SaveChanges();
+
+					Console.WriteLine(DateTime.Now);
+				}
+			}
+		}
+	}
+}

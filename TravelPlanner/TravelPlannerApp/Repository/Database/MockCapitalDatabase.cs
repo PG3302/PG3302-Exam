c3ee4135bb047968cebbd02bc13a2c5324cf9337
Vegard Falldalen <vegfall@gmail.com>
2023-11-09 14:12:23 +0100

    Added mock trip and capital database

---
+++ TravelPlanner/TravelPlannerApp/Repository/Database/MockCapitalDatabase.cs
@@ -0,0 +1,57 @@
+﻿using TravelPlanner.TravelPlannerApp.Data.Log;
+using TravelPlanner.TravelPlannerApp.Data.Objects;
+
+namespace TravelPlanner.TravelPlannerApp.Repository.Database
+{
+    internal class MockCapitalDatabase : IMockDatabase
+    {
+        private readonly List<Capital> _capitalList = new();
+        private long _mockCurrentId = 0;
+
+        public MockCapitalDatabase()
+        {
+            AddCapital(new("Oslo", new(10, 10), Data.DataType.Continent.Europe));
+            AddCapital(new("Washington", new(-30, -30), Data.DataType.Continent.NorthAmerica));
+            AddCapital(new("Cape Town", new(10, -50), Data.DataType.Continent.Africa));
+        }
+
+        public Capital AddCapital(Capital capital)
+        {
+            ConnectDatabase();
+
+            //Mock
+            capital.Id = _mockCurrentId;
+            _mockCurrentId++;
+            _capitalList.Add(capital);
+
+            Logger.LogInfo($"Capital {capital.Name} added.");
+
+            //Statement
+            return (capital);
+        }
+
+        public Capital? GetUserByName(string name)
+        {
+            Capital? requestedCapital = _capitalList.Find(n => n.Name == name);
+
+            return requestedCapital;
+        }
+
+        public Capital? GetUserById(long id)
+        {
+            Capital? requestedCapital = _capitalList.Find(i => i.Id == id);
+
+            return requestedCapital;
+        }
+
+        public void ConnectDatabase()
+        {
+            Console.WriteLine("Capital Database Connected...");
+        }
+
+        public void DisconnectDatabase()
+        {
+            Console.WriteLine("Capital Database Disconnected...");
+        }
+    }
+}
---
+++ TravelPlanner/TravelPlannerApp/Repository/Database/MockTripDatabase.cs
@@ -0,0 +1,55 @@
+﻿using TravelPlanner.TravelPlannerApp.Data.Log;
+using TravelPlanner.TravelPlannerApp.Data.Objects;
+
+namespace TravelPlanner.TravelPlannerApp.Repository.Database
+{
+    internal class MockTripDatabase : IMockDatabase
+    {
+        private readonly List<Trip> _tripList = new();
+        private long _mockCurrentId = 0;
+
+        public MockTripDatabase()
+        {
+
+        }
+
+        public Trip AddTrip(Trip trip)
+        {
+            ConnectDatabase();
+
+            //Mock
+            trip.Id = _mockCurrentId;
+            _mockCurrentId++;
+            _tripList.Add(trip);
+
+            Logger.LogInfo($"Trip {trip.StartingCapital} -> {trip.DestinationCapital} added.");
+
+            //Statement
+            return (trip);
+        }
+
+        public Trip? GetTripByUsername(string username)
+        {
+            Trip? requestedTrip = _tripList.Find(t => t.User.Username == username);
+
+            return requestedTrip;
+        }
+
+        public Trip? GetUserById(long id)
+        {
+            Trip? requestedTrip = _tripList.Find(i => i.Id == id);
+
+            return requestedTrip;
+        }
+
+        public void ConnectDatabase()
+        {
+            Console.WriteLine("Trip Database Connected...");
+        }
+
+        public void DisconnectDatabase()
+        {
+            Console.WriteLine("Trip Database Disconnected...");
+        }
+    }
+}

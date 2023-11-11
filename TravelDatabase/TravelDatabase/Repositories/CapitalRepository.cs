f4cacea35881c65f03f45469310d61610b21ac3c
LHL <lola003@student.kristiania.no>
2023-11-11 23:15:22 +0100

    Added UnitTests & further repos

--- TravelDatabase/TravelDatabase/Repositories/CapitalRepository.cs
+++ TravelDatabase/TravelDatabase/Repositories/CapitalRepository.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using TravelDatabase.DataAccess.SqLite;
 using TravelDatabase.Entities;
 
 namespace TravelDatabase.Repositories {
 	public class CapitalRepository {
+		public static int AddCapital(string name, Continent continent, decimal longitude, decimal latitude) {
+			using var travelDbContext = new TravelDbContext();
+			Capital capital = new Capital();
+			{
+				capital.CapitalName = name;
+				capital.Continent = continent;
+				capital.Longitude = longitude;
+				capital.Latitude = latitude;
+			}			
+			travelDbContext.Capital.Add(capital);
+			travelDbContext.SaveChanges();
+
+			return capital.Id;
+		}
 
 		public List<Capital> GetAllCapitals() {
 			using var travelDbContext = new TravelDbContext();
@@ -19,7 +34,12 @@ namespace TravelDatabase.Repositories {
 		}
 		public void EditCapital(int capitalId , string name , Continent continent , decimal longitude , decimal latitude) {
 			using var travelDbContext = new TravelDbContext();
-			var oldCapital = travelDbContext.Capital.First(capital => capital.Id == capitalId); 
+			var oldCapital = travelDbContext.Capital.First(capital => capital.Id == capitalId);
+			oldCapital.CapitalName = name;
+			oldCapital.Continent = continent;
+			oldCapital.Longitude = longitude;
+			oldCapital.Latitude = latitude;
+			travelDbContext.SaveChanges();
 		}
 	}
 }
--- TravelDatabase/TravelDbTest/UnitTest1.cs
+++ TravelDatabase/TravelDbTest/UnitTest1.cs
@@ -9,8 +9,8 @@ namespace TravelDbTest {
 		}
 
 		[Test]
-		public void TestAddUser() {
-			int userId = UserRepository.AddUser("test", new TravelDatabase.Entities.Capital(), 0);
+		public void AddUserTest() {
+			int userId = UserRepository.AddUser("test", new CapitalRepository().GetAllCapitals().First(), 0);
 			Assert.That(1 , Is.EqualTo(userId));
 		} // test DB can be found \\TravelDatabase\TravelDbTest\bin\Debug\net6.0\Resources
 
@@ -18,5 +18,17 @@ namespace TravelDbTest {
 		public void AddUserOutsideAdminSpecsFails() {
 			Assert.Throws<Exception>(() => UserRepository.AddUser("test" , new TravelDatabase.Entities.Capital() , 3)); //throws<exception> thinks it's recieving an int. lambda fix
 		}
+		[Test]
+		public void AddCapitalTest() {
+			int capitalId = CapitalRepository.AddCapital("test" , 0 , 1 , 2);
+			var allCapitals = new CapitalRepository().GetAllCapitals();
+			Assert.That(allCapitals.Any(capital => capital.CapitalName.Contains("test")));
+		}
+		/*[Test]
+		public void EditCapitalTest() {
+			int capitalId = CapitalRepository.EditCapital(){
+			}
+		}*/
+
 	}
 }

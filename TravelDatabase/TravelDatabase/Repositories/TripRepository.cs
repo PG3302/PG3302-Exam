2aea97fc53c071cc9ee5cecf0a165b251df96f55
LHL <lola003@student.kristiania.no>
2023-11-11 17:27:06 +0100

    Added methods to TripRepo

--- TravelDatabase/TravelDatabase/Repositories/TripRepository.cs
+++ TravelDatabase/TravelDatabase/Repositories/TripRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,19 +10,24 @@ using TravelDatabase.Entities;
 namespace TravelDatabase.Repositories {
 	public class TripRepository {
 		public static void AddTrip(int userId , int departlocation , int arrivalLocation) {
-			using TravelDbContext db = new();
+			using var travelDbContext = new TravelDbContext();
 			Trip trip = new() {
 				UserId = userId ,
 				DepartureId = departlocation ,
 				ArrivalId = arrivalLocation ,
 			};
-			db.Add(trip);
-			db.SaveChanges();
+			travelDbContext.Add(trip);
+			travelDbContext.SaveChanges();
 		}
-		 public List<Trip> GetAllUserTrips(int userId) {
+		public List<Trip> GetAllUserTrips(int userId) {
 			using var travelDbContext = new TravelDbContext();
-				return travelDbContext.Trip.Where(trip => trip.UserId == userId ).ToList();
-
+			return travelDbContext.Trip.Where(trip => trip.UserId == userId ).ToList();
+		}
+		public void DeleteTrip(int tripId) {
+			using var travelDbContext = new TravelDbContext();
+			var trip = 	travelDbContext.Trip.First(trip => trip.Id == tripId);
+			travelDbContext.Trip.Remove(trip);
+			travelDbContext.SaveChanges();
 		}
 	}
 }

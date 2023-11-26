f5b1230504a3d71f047bf638abb71b135e7c85c3
Vegard Falldalen <vegfall@gmail.com>
2023-11-26 12:49:51 +0100

    Added datatypes, handlers and log

---
+++ TravelPlanner/TravelPlannerApp/Data/DataType/Coordinate.cs
@@ -0,0 +1,28 @@
+﻿namespace TravelPlanner.TravelPlannerApp.Data.DataType
+{
+    public class Coordinate
+    {
+        public double Longitude { get; private set; }
+        public double Latitude { get; private set; }
+
+        public Coordinate(double longitude, double latitude)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+
+        //Find distance between two coordinates
+        public static double operator -(Coordinate coordinate1, Coordinate coordinate2)
+        {
+            double longitudePower = Math.Pow(coordinate1.Longitude - coordinate2.Longitude, 2);
+            double latitudePower = Math.Pow(coordinate1.Latitude - coordinate2.Latitude, 2);
+
+            return Math.Sqrt(longitudePower + latitudePower);
+        }
+
+        public override string ToString()
+        {
+            return $"Longitude: {Longitude}, Latitude: {Latitude}";
+        }
+    }
+}
---
+++ TravelPlanner/TravelPlannerApp/Data/Handlers/FileHandler.cs
@@ -0,0 +1,52 @@
+﻿using TravelPlanner.TravelPlannerApp.Data.Log;
+
+namespace TravelPlanner.TravelPlannerApp.Data.Handlers
+{
+    public static class FileHandler
+    {
+        private static readonly string _filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        private static readonly string _fileName = "TravelPlannerLog.txt";
+        private static readonly string _defaultFullPath = Path.Combine(_filePath, _fileName);
+        private static readonly object _fileLock = new();
+
+        public static void WriteToFile(string message, string? path = null)
+        {
+            string filePath = path ?? _defaultFullPath;
+
+            lock (_fileLock)
+            {
+                using StreamWriter output = new(filePath);
+                output.WriteLine(message);
+            }
+        }
+
+        public static string? ReadFromFile(string? path = null)
+        {
+            string filePath = path ?? _defaultFullPath;
+
+            try
+            {
+                lock (_fileLock)
+                {
+                    using StreamReader? content = new(filePath);
+                    return content.ReadToEnd();
+                }
+            }
+            catch (IOException error)
+            {
+                Logger.LogError("Error when reading from file.", error);
+                return null;
+            }
+        }
+
+        public static void DeleteFile(string? path = null)
+        {
+            string filePath = path ?? _defaultFullPath;
+
+            lock (_fileLock)
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}

9c6ca61fe1e6256588256c0f0f5ba7b037d8c249
LHL <lola003@student.kristiania.no>
2023-11-27 16:31:03 +0100

    Data folder move
    From TravelPlanner to TravelDatabase

---
+++ TravelDatabase/TravelDatabase/Data/DataType/Continent.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelDatabase.Data.DataType {
+	public enum Continent {
+		None,
+		Africa,
+		Antarctica,
+		Asia,
+		Oceanaria,
+		Europe,
+		NorthAmerica,
+		SouthAmerica,
+		CentralAmerica,
+	}
+}

8e8e1a1af176a83169bf43f451ccd89133240619
LHL <lola003@student.kristiania.no>
2023-11-27 21:10:24 +0100

    Capital repo new Model use

--- TravelDatabase/TravelDatabase/Repositories/CapitalRepository.cs
+++ TravelDatabase/TravelDatabase/Repositories/CapitalRepository.cs
@@ -3,46 +3,54 @@ using TravelDatabase.Entities;
 using TravelDatabase.Models;
 using Microsoft.EntityFrameworkCore;
 using TravelDatabase.Data.DataType;
+using System.Reflection.Metadata.Ecma335;
 
 
 namespace TravelDatabase.Repositories
 {
     public class CapitalRepository {
-		public static int AddCapital(string name, Continent continent, decimal longitude, decimal latitude) {
+		public static int AddCapital(CapitalModel newCapital) {
 			using TravelDbContext travelDbContext = new();
+			if (travelDbContext.Capital.Any(c=>c.CapitalName == newCapital.Name)) {
+			}
 			Capital capital = new();
 			{
-				capital.CapitalName = name;
-				capital.Continent = continent;
-				capital.Longitude = longitude;
-				capital.Latitude = latitude;
+				capital.CapitalName = newCapital.Name;
+				capital.Continent = newCapital.Continent;
+				capital.Longitude = (decimal)newCapital.Coordinate.Longitude;
+				capital.Latitude = (decimal)newCapital.Coordinate.Latitude;
 			}			
 			travelDbContext.Capital.Add(capital);
 			travelDbContext.SaveChanges();
 
 			return capital.Id;
 		}
-		public List<Capital> GetCapitalAll() {
-			using TravelDbContext travelDbContext = new();
-			return travelDbContext.Capital.ToList();
-		}
-		public Capital GetCapitalById(int id) {
-			using TravelDbContext travelDbContext = new();
-			return travelDbContext.Capital.First(capital => capital.Id == id);
-		}
-		public Capital GetCapitalByName(string name)
+
+		public List<CapitalModel> GetCapitalAll() 
 		{
 			using TravelDbContext travelDbContext = new();
-			return travelDbContext.Capital.First(capital => capital.CapitalName == name);
+			List<Capital> capitals = travelDbContext.Capital.ToList();
+			return capitals.Select(c => MapCapital(c)).ToList();
 		}
-        public List<Capital> GetCapitalsByContinent(Continent continent)
+
+		public CapitalModel? GetCapitalById(int capitalId) 
+			{
+			using TravelDbContext travelDbContext = new();
+			Capital? capital = travelDbContext.Capital.Find(capitalId);
+			return MapCapital(capital);
+		}
+
+		public CapitalModel GetCapitalByName(string name)
+		{
+			using TravelDbContext travelDbContext = new();
+			Capital? capital = travelDbContext.Capital.First(capital => capital.CapitalName == name);
+			return MapCapital(capital);
+		}
+        public List<CapitalModel> GetCapitalsByContinent(Continent continent)
         {
             using TravelDbContext travelDbContext = new TravelDbContext();
-
-            return travelDbContext.Capital
-                .Include(c => c.Continent)
-                .Where(c => c.Continent == continent)
-                .ToList();
+			List<Capital> capitals = travelDbContext.Capital.Include(c => c.Continent).Where(c => c.Continent == continent).ToList();
+            return capitals.Select(c=>MapCapital(c)).ToList();
         }
         public void EditCapital(int capitalId , string name , Continent continent , decimal longitude , decimal latitude) {
 			using TravelDbContext travelDbContext = new();
@@ -53,5 +61,13 @@ namespace TravelDatabase.Repositories
 			oldCapital.Latitude = latitude;
 			travelDbContext.SaveChanges();
 		}
+
+		private CapitalModel? MapCapital(Capital capital) {
+			if (capital == null) {
+				return null;
+			}
+			return new CapitalModel(capital.Id , capital.CapitalName , capital.Longitude , capital.Latitude , capital.Continent);
+		}
+		
 	}
 }

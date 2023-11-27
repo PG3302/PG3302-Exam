101a78c3a73b3c3410452dc2922bcb9828e647c3
LHL <lola003@student.kristiania.no>
2023-11-27 17:22:00 +0100

    User Entity corrections
    added email removed city

--- TravelDatabase/TravelDatabase/Entities/User.cs
+++ TravelDatabase/TravelDatabase/Entities/User.cs
@@ -9,8 +9,7 @@ namespace TravelDatabase.Entities {
 	public class User {
 		public int Id { get; set; } //PK
 		public string? Name { get; set; }
-		public Capital? City {	get; set; } //Navigation property
-		public int CityId {	get; set; } //FK to Capital
+		public string? Email { get; set; }
 		public int Admin { get; set; }
 		public ICollection<Trip>? Trip { get; set; } //Navigation property
 	}

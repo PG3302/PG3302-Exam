c0be37d7f19f458910543a504efe481048aaf1ea
LHL <lola003@student.kristiania.no>
2023-11-28 21:05:02 +0100

    Added ref for DB m innhold

--- TravelDatabase/TravelDatabase/Program.cs
+++ TravelDatabase/TravelDatabase/Program.cs
@@ -1,10 +1,13 @@
 ﻿using TravelDatabase.Models;
 using TravelDatabase.Repositories;
 
+// Database saves here: \\TravelDatabase\TravelDatabase\bin\Debug\net6.0\Resources
+
 namespace TravelDatabase {
 	public class Program {
 		static void Main() {
 			InitDatabase.InitFromCsv();
+
 			UserRepository userRepo = new UserRepository();
 			userRepo.AddUser(new UserModel(null , "TestUser" , "testUser@Test.com" , false));
 			userRepo.AddUser(new UserModel(null , "TestAdmin" , "testAdmin@Test.com" , true));

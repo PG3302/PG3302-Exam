583a6e10109fc62695e0ca873ed4f893c5656634
Vegard Falldalen <vegfall@gmail.com>
2023-11-28 21:36:39 +0100

    Updated database initialization

--- TravelPlanner/TravelPlannerApp/Controller/MenuControllers/ListMenus.cs
+++ TravelPlanner/TravelPlannerApp/Controller/MenuControllers/ListMenus.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TravelPlanner.TravelPlannerApp.Service;
-
-namespace TravelPlanner.TravelPlannerApp.Controller.MenuControllers
+﻿namespace TravelPlanner.TravelPlannerApp.Controller.MenuControllers
 {
     internal class ListMenus : UIController
     {
         internal void ListMenu()
         {
             _menuController.AddMenu("Back.", MainMenu);
-            //_menuController.AddMenu("Filter", )
+            _menuController.AddMenu("Filter", ListMenuFilter);
             _menuController.AddList(_capitalService.GetCapitalAll(), MainMenu);
             _menuController.RunMenu("List of travel locations...", MainMenu);
         }
